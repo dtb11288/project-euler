62ddca788d2493817ca18200b3d73cac52c65f0b
binh <dau.thanh.binh@gmail.com>
2016-03-31 11:31:05 +0700

    update some stupid definations

--- 2/2.hs
+++ 2/2.hs
@@ -1,6 +1,9 @@
+fibs :: [Integer]
 fibs = 1 : 2 : zipWith (+) (tail fibs) fibs
 
+solve :: Integer
 solve = sum $ filter (\x -> x `mod` 2 == 0) (takeWhile (\x -> x < 4000000) fibs)
 
+main :: IO ()
 main = do
-   print $ solve
+    print $ solve
